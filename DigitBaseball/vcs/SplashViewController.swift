0c67198d58e758be82a0a8df874ed5330416aa1e
Zoonmy <pjm2571@naver.com>
2024-12-19 12:40:08 +0900

    feat: 스플래시 구현

--- DigitBaseball/vcs/SplashViewController.swift
+++ DigitBaseball/vcs/SplashViewController.swift
@@ -7,23 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 2초 후에 메인 화면으로 전환
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-//            if let user = Auth.auth().currentUser {
-//                print("현재 로그인된 사용자: \(user.email ?? "이메일 없음")")
-//                // 로그인 되어 있으면 메인화면으로 이동
-//                self.showScreen(withIdentifier: "mainTabBarController")
-//            } else {
-//                print("로그인된 사용자가 없습니다.")
-//                // 로그인 되어 있지 않으면 인트로 화면으로 이동
-//                self.showScreen(withIdentifier: "introViewController")
-//            }
-            self.showScreen(withIdentifier: "MainViewController")
+            self.showScreen(withIdentifier: "mainViewController")
         }
     }
     
@@ -33,6 +23,8 @@ class ViewController: UIViewController {
             mainVC.modalTransitionStyle = .crossDissolve // 부드러운 전환 효과
             mainVC.modalPresentationStyle = .fullScreen
             self.present(mainVC, animated: true, completion: nil)
+        } else {
+            print("error")
         }
     }
 
